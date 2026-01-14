55022a70af686583938f4eda31d65fc037e712a9
Tania Macheieva <120329506+tania-macheieva@users.noreply.github.com>
2026-01-14 17:37:23 +0200

    Remove TotalSeats property from Hall class
    Removed TotalSeats property from Hall class.

--- Core/Entities/Hall.cs
+++ Core/Entities/Hall.cs
@@ -4,7 +4,6 @@ public class Hall
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public int TotalSeats { get; set; }
 
     public virtual ICollection<Seat> Seats { get; set; } = new List<Seat>();
 }   
