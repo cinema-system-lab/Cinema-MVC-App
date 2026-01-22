ac53510426bfaea0c5306417b8768cc6968bd7a6
Artem <supercrash2506@gmail.com>
2026-01-22 14:56:21 +0200

    refactor: separate enums into files

---
+++ Core/Enums/GenreType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Core.Enums
+{
+    // Той самий бітовий енам, про який казав тімлід
+    [Flags]
+    public enum GenreType
+    {
+        None = 0,
+        Action = 1,
+        Drama = 2,
+        Comedy = 4,
+        Horror = 8,
+        SciFi = 16,
+        Documentary = 32,
+        Thriller = 64,
+        Fantasy = 128,
+        Animation = 256
+        // Можна додавати далі: 512, 1024...
+    }
+}
