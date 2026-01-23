7fcb9ad98b07e067c9a24710eef65a5f56b1106d
Максим Гладун <123461058+Max4423@users.noreply.github.com>
2026-01-23 15:34:17 +0200

    Add seat DTOs, service interface, mapping, and validator
    Introduces SeatDTO and SeatGenerationDTO for seat data transfer, ISeatService interface for seat operations, SeatProfile for AutoMapper configuration, and SeatGenerationValidator for validating seat generation input.

---
+++ Core/DTOs/SeatGenerationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.DTOs;
+
+public class SeatGenerationDTO
+{
+    public int HallId { get; set; }
+    public int Rows { get; set; }
+    public int SeatsPerRow { get; set; }
+}
---
+++ Core/Interfaces/Services/ISeatService.cs
@@ -0,0 +1,12 @@
+﻿using Core.DTOs;
+
+namespace Core.Interfaces.Services;
+
+public interface ISeatService
+{
+    Task<List<SeatDTO>> GetSeatsByHallIdAsync(int hallId);
+    
+    Task GenerateSeatsAsync(SeatGenerationDTO generationDto);
+    
+    Task DeleteSeatAsync(int id);
+}
