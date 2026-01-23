603a495bf3f8076509053102cc90abd45f2c6bb3
stepan-redka <stepanredka17@gmail.com>
2026-01-23 13:49:39 +0200

    from the develop branch: merged Enums folder

---
+++ Core/Enums/PaymentStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core.Enums
+{
+    public enum PaymentStatus : byte
+    {
+        Pending = 1,
+        Success = 2,
+        Failed = 3
+    }
+}
---
+++ Core/Enums/SeatType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Core.Enums
+{
+    public enum SeatType : byte
+    {
+        Regular = 1,
+        Premium = 2
+    }
+}
