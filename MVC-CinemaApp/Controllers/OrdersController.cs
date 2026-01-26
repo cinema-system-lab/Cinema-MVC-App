a547049eba1ab71e040412a498422cd1a849e7ef
Bohdan <bogdanpelyak@gmail.com>
2026-01-26 15:45:19 +0200

    add DTO structure

---
+++ MVC-CinemaApp/Controllers/OrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema_MVC_App.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
