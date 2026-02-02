b10dde260249df934c672ac8c8df1ef5f5f08638
stepan-redka <stepanredka17@gmail.com>
2026-02-02 17:04:14 +0200

    feat: add LoginValidator with email and password validation

---
+++ Core/Validators/LoginValidator.cs
@@ -0,0 +1,17 @@
+using Core.DTOs;
+using FluentValidation;
+
+namespace Core.Validators;
+
+public class LoginValidator : AbstractValidator<LoginDTO>
+{
+    public LoginValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required");
+    }
+}
