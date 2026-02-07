3c96733d820f4c578d051d4efb8c34df172fd397
Artem <supercrash2506@gmail.com>
2026-02-07 23:01:54 +0200

    UI: Refactor layout, unified Home and Movies

--- MVC-CinemaApp/Controllers/HomeController.cs
+++ MVC-CinemaApp/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Cinema_MVC_App.Models;
+using Core.Interfaces.Services;
 
 namespace Cinema_MVC_App.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IMovieService _movieService; // Поле для сервиса
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IMovieService movieService)
     {
         _logger = logger;
+        _movieService = movieService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        // Получаем все фильмы для отображения на витрине
+        var movies = await _movieService.GetAllMoviesAsync();
+        return View(movies);
     }
 
     public IActionResult Privacy()
