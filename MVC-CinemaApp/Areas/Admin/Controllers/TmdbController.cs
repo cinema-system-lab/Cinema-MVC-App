68abde33fe602b62f31c76fa501b95767191922e
tania-macheieva <macheieva.tetiana@gmail.com>
2026-02-11 16:24:35 +0200

    feat: Implemented UI for TMDB in ADMIN.

--- MVC-CinemaApp/Areas/Admin/Controllers/TmdbController.cs
+++ MVC-CinemaApp/Areas/Admin/Controllers/TmdbController.cs
@@ -6,29 +6,29 @@ namespace Cinema_MVC_App.Areas.Admin.Controllers;
 
 public class TmdbController : BaseAdminController
 {
-    private readonly ITmdbService _tmdbService;
+        private readonly ITmdbService _tmdbService;
 
-    public TmdbController(ITmdbService tmdbService)
-    {
-        _tmdbService = tmdbService;
-    }
+        public TmdbController(ITmdbService tmdbService)
+        {
+            _tmdbService = tmdbService;
+        }
 
-    // GET: /AdminTmdb
-    public async Task<IActionResult> Index(string? searchQuery)
-    {
-        if (string.IsNullOrWhiteSpace(searchQuery))
+        // GET: /Admin/Tmdb
+        public async Task<IActionResult> Index(string? searchQuery)
         {
-            var popular = await _tmdbService.GetPopularMoviesAsync();
-            ViewBag.Title = "Popular Movies";
-            return View(popular);
+            if (string.IsNullOrWhiteSpace(searchQuery))
+            {
+                var popular = await _tmdbService.GetPopularMoviesAsync();
+                ViewBag.Title = "Popular Movies";
+                return View(popular);
+            }
+            else
+            {
+                // Якщо щось ввели - шукаємо
+                var searchResults = await _tmdbService.SearchMoviesAsync(searchQuery);
+                ViewBag.Title = $"Search results for '{searchQuery}'";
+                ViewBag.SearchQuery = searchQuery; 
+                return View(searchResults);
+            }
         }
-        else
-        {
-            // Якщо щось ввели - шукаємо
-            var searchResults = await _tmdbService.SearchMoviesAsync(searchQuery);
-            ViewBag.Title = $"Search results for '{searchQuery}'";
-            ViewBag.SearchQuery = searchQuery; 
-            return View(searchResults);
-        }
-    }
 }
